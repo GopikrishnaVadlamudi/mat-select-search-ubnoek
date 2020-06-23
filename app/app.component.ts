e94f862118d6cf7427ec8f033d9f89c874e44865
GopikrishnaVadlamudi <gopi.vadlamudi10@gmail.com>
2020-06-23 22:10:50 +0530

    2 files modified

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,65 +1,89 @@
-import {Component, ViewChild, AfterViewInit} from '@angular/core';
-import {VERSION} from '@angular/material';
-import { FormControl } from '@angular/forms';
-import {MatSelect} from '@angular/material';
+import { Component, ViewChild, AfterViewInit } from "@angular/core";
+import { VERSION } from "@angular/material";
+import { FormControl } from "@angular/forms";
+import { MatSelect } from "@angular/material";
 
-import { ReplaySubject } from 'rxjs/ReplaySubject';
-import { Subject } from 'rxjs/Subject';
-import { take, takeUntil } from 'rxjs/operators';
+import { ReplaySubject } from "rxjs/ReplaySubject";
+import { Subject } from "rxjs/Subject";
+import { take, takeUntil } from "rxjs/operators";
 
 interface Bank {
- id: string;
- name: string;
+  id: string;
+  name: string;
+  pokemon: Pokemon[];
 }
-
+interface Pokemon {
+  value: string;
+  viewValue: string;
+}
 @Component({
-  selector: 'material-app',
-  templateUrl: 'app.component.html'
+  selector: "material-app",
+  templateUrl: "app.component.html"
 })
-export class AppComponent { 
+export class AppComponent {
   version = VERSION;
 
   /** control for the selected bank */
   public bankCtrl: FormControl = new FormControl();
 
-   /** control for the MatSelect filter keyword */
+  /** control for the MatSelect filter keyword */
   public bankFilterCtrl: FormControl = new FormControl();
 
-    /** control for the selected bank for multi-selection */
+  /** control for the selected bank for multi-selection */
   public bankMultiCtrl: FormControl = new FormControl();
 
-   /** control for the MatSelect filter keyword multi-selection */
+  /** control for the MatSelect filter keyword multi-selection */
   public bankMultiFilterCtrl: FormControl = new FormControl();
 
   /** list of banks */
   private banks: Bank[] = [
-    {name: 'Bank A (Switzerland)', id: 'A'},
-    {name: 'Bank B (Switzerland)', id: 'B'},
-    {name: 'Bank C (France)', id: 'C'},
-    {name: 'Bank D (France)', id: 'D'},
-    {name: 'Bank E (France)', id: 'E'},
-    {name: 'Bank F (Italy)', id: 'F'},
-    {name: 'Bank G (Italy)', id: 'G'},
-    {name: 'Bank H (Italy)', id: 'H'},
-    {name: 'Bank I (Italy)', id: 'I'},
-    {name: 'Bank J (Italy)', id: 'J'},
-    {name: 'Bank K (Italy)', id: 'K'},
-    {name: 'Bank L (Germany)', id: 'L'},
-    {name: 'Bank M (Germany)', id: 'M'},
-    {name: 'Bank N (Germany)', id: 'N'},
-    {name: 'Bank O (Germany)', id: 'O'},
-    {name: 'Bank P (Germany)', id: 'P'},
-    {name: 'Bank Q (Germany)', id: 'Q'},
-    {name: 'Bank R (Germany)', id: 'R'} 
-  ]
+    {
+      name: "Bank A (Switzerland)",
+      id: "A",
+      pokemon: [
+        { value: "bulbasaur-0", viewValue: "Bulbasaur0" },
+        { value: "oddish-1", viewValue: "Oddish0" },
+        { value: "bellsprout-2", viewValue: "Bellsprout0" }
+      ]
+    },
+    {
+      name: "Bank B (Switzerland)",
+      id: "B",
+      pokemon: [
+        { value: "bulbasaur-1", viewValue: "Bulbasaur1" },
+        { value: "oddish-2", viewValue: "Oddish1" },
+        { value: "bellsprout-3", viewValue: "Bellsprout1" }
+      ]
+    },
+    {
+      name: "Bank C (France)",
+      id: "C",
+      pokemon: [
+        { value: "bulbasaur-4", viewValue: "Bulbasaur2" },
+        { value: "oddish-5", viewValue: "Oddish2" },
+        { value: "bellsprout-6", viewValue: "Bellsprout2" }
+      ]
+    },
+    {
+      name: "Bank D (France)",
+      id: "D",
+      pokemon: [
+        { value: "bulbasaur-7", viewValue: "Bulbasaur" },
+        { value: "oddish-8", viewValue: "Oddish" },
+        { value: "bellsprout-9", viewValue: "Bellsprout" }
+      ]
+    }
+  ];
 
   /** list of banks filtered by search keyword */
   public filteredBanks: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
 
   /** list of banks filtered by search keyword for multi-selection */
-  public filteredBanksMulti: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(1);
+  public filteredBanksMulti: ReplaySubject<Bank[]> = new ReplaySubject<Bank[]>(
+    1
+  );
 
-  @ViewChild('singleSelect') singleSelect: MatSelect; 
+  @ViewChild("singleSelect") singleSelect: MatSelect;
 
   /** Subject that emits when the component has been destroyed. */
   private _onDestroy = new Subject<void>();
@@ -83,7 +107,6 @@ export class AppComponent {
       .subscribe(() => {
         this.filterBanksMulti();
       });
-
   }
 
   ngAfterViewInit() {
@@ -100,12 +123,15 @@ export class AppComponent {
    */
   private setInitialValue() {
     this.filteredBanks
-      .pipe(take(1), takeUntil(this._onDestroy))
-      .subscribe(() => {          
-        // setting the compareWith property to a comparison function 
-        // triggers initializing the selection according to the initial value of 
+      .pipe(
+        take(1),
+        takeUntil(this._onDestroy)
+      )
+      .subscribe(() => {
+        // setting the compareWith property to a comparison function
+        // triggers initializing the selection according to the initial value of
         // the form control (i.e. _initializeSelection())
-        // this needs to be done after the filteredBanks are loaded initially 
+        // this needs to be done after the filteredBanks are loaded initially
         // and after the mat-option elements are available
         this.singleSelect.compareWith = (a: Bank, b: Bank) => a.id === b.id;
       });
@@ -146,7 +172,6 @@ export class AppComponent {
       this.banks.filter(bank => bank.name.toLowerCase().indexOf(search) > -1)
     );
   }
-
 }
 
 /**
